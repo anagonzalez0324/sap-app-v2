ad3ef98854332b4a9f8db524411bc5b753bee209
anagonzalez0324 <anagonzalezntx@gmail.com>
2024-01-20 12:52:07 -0600

    rendered dynamic info

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Buffalo Boys',
+    'imprint_location': 'Left Chest',
+    'price': '$100.00'
+  },
+  {
+    'id': 1,
+    'title': 'Reliant Logo',
+    'imprint_location': 'Left Chest',
+    'price': '$100.00'
+  },
+  {
+    'id': 1,
+    'title': 'T Parker Hats',
+    'imprint_location': 'Left Chest',
+    'price': '$100.00'
+  },
+  {
+    'id': 1,
+    'title': 'Big D Welding',
+    'imprint_location': 'Left Chest',
+    'price': '$100.00'
+  }
+]
+
 @app.route("/")
-def hellow_world():
-  return "Hello Ana"
+def hello():
+  return render_template('home.html')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True, port=8080)
